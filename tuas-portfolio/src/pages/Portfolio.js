dcb0e10bdeb2e15626f23f209d7a7b006d762e08
TuaTuivai <TuaMTuivai@gmail.com>
2023-03-21 16:30:16 -0700

    fixed issues with portfolio imgs and working on styling

--- tuas-portfolio/src/pages/Portfolio.js
+++ tuas-portfolio/src/pages/Portfolio.js
@@ -1,71 +1,64 @@
-import React from 'react';
-import '../styles/Portfolio.css'
+import React from "react";
+import "../styles/Portfolio.css";
 
-export default function Portfolio() {
-  const projects = [
-    {
-      id: 1,
-      title: 'Project 1',
-      description: 'Description of Project 1',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project1',
-    },
-    {
-      id: 2,
-      title: 'Project 2',
-      description: 'Description of Project 2',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project2',
-    },
-    {
-      id: 3,
-      title: 'Project 3',
-      description: 'Description of Project 3',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project3',
-    },
-    {
-      id: 4,
-      title: 'Project 4',
-      description: 'Description of Project 4',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project4',
-    },
-    {
-      id: 5,
-      title: 'Project 5',
-      description: 'Description of Project 5',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project5',
-    },
-    {
-      id: 6,
-      title: 'Project 6',
-      description: 'Description of Project 6',
-      imageUrl: 'https://via.placeholder.com/150',
-      demoUrl: 'https://example.com/demo',
-      githubUrl: 'https://github.com/example/project6',
-    },
-  ];
+const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project1",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project2",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project2",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project2",
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project2",
+  },
+  {
+    id: 6,
+    title: "Project 6",
+    image: "https://via.placeholder.com/200x200",
+    github: "https://github.com/yourusername/project2",
+  },
+  
+  // Add more projects here
+];
 
+const Portfolio = () => {
   return (
-<div className='portfolio'>
-  <h1>Portfolio</h1>
-  {projects.map((project) => (
-    <div key={project.id}>
-      <h3>{project.title}</h3>
-      <img src={project.imageUrl} alt={project.title} />
-      <p>{project.description}</p>
-      <a href={project.demoUrl}>Demo</a> | <a href={project.githubUrl}>GitHub</a>
+    <div className="portfolio">
+      <h1 className="portfolio-title">My Portfolio</h1>
+      <div className="projects">
+        {projects.map((project) => (
+          <div key={project.id} className="project">
+            <img src={project.image} alt={project.title} className="project-image" />
+            <h3 className="project-title">{project.title}</h3>
+            <a href={project.github} className="project-link" target="_blank" rel="noopener noreferrer">
+              View on GitHub
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-</div>
-
   );
-}
+};
 
+export default Portfolio;
--- tuas-portfolio/src/pages/Resume.js
+++ tuas-portfolio/src/pages/Resume.js
@@ -1,28 +1,70 @@
 import React from 'react';
+import '../styles/Resume.css';
 
 export default function Resume() {
-  const proficiencies = [
-    'JavaScript',
-    'React',
+  const frontEndSkills = [
     'HTML',
     'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Responsive Design',
+    'Bootstrap',
+    'Sass',
+  ];
+
+  const backEndSkills = [
     'Node.js',
     'Express.js',
     'MongoDB',
-    'Git',
+    'SQL',
+    'RESTful APIs',
+    'Authentication & Authorization',
+    'Version Control (Git)',
   ];
 
+  const bootcamp = {
+    name: 'Full Stack Web Developer Bootcamp',
+    institution: 'UCLA EXT Bootcamp',
+    duration: '6 months',
+    description:
+      'An intensive, project-based full stack web development bootcamp covering a wide range of modern web technologies and best practices.',
+  };
+
   return (
-    <div>
+    <div className="resume-container">
       <h1>Resume</h1>
-      <p>Download my resume <a href="/resume.pdf" download>here</a>.</p>
-      <h2>Proficiencies</h2>
-      <ul>
-        {proficiencies.map((proficiency) => (
-          <li key={proficiency}>{proficiency}</li>
-        ))}
-      </ul>
+      <p>
+        Download my resume <a href="/resume.pdf" download>here</a>.
+      </p>
+      <section>
+        <h2>Education</h2>
+        <h3>{bootcamp.name}</h3>
+        <h4>{bootcamp.institution}</h4>
+        <h5>{bootcamp.duration}</h5>
+        <p>{bootcamp.description}</p>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <div className="skills-container">
+          <div className="skills-section">
+            <h3>Front End</h3>
+            <ul>
+              {frontEndSkills.map((skill) => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="skills-section">
+            <h3>Back End</h3>
+            <ul>
+              {backEndSkills.map((skill) => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
