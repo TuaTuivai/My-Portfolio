53b769704c1e9c24779f5454a33633bf3dff0d6b
TuaTuivai <TuaMTuivai@gmail.com>
2023-03-27 01:06:09 -0700

    fixed footer before build

--- tuas-portfolio/src/components/Footer.js
+++ tuas-portfolio/src/components/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import '../styles/Footer.css';
-import GithubLogo from '../styles/Images/icons8-github-64.png';
-import twitterLogo from '../styles/Images/icons8-twitter-50.png';
-import linkedinLogo from '../styles/Images/linkedin.png';
+import GithubLogo from '../styles/Images/githubLogo.png';
+import stackoverflowLogo from '../styles/Images/stackOverflowLogo.png';
+import linkedinLogo from '../styles/Images/linkedInLogo.png';
 import '../styles/fonts.css';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://www.facebook.com">
-        <img src={GithubLogo} alt="Github" />
-        <span>Facebook</span>
+      <a href="https://stackoverflow.com/users/21499336/tua-tuivai">
+        <img src={stackoverflowLogo} alt="stackOverflow" />
+        <span>Stack overFlow</span>
       </a>
-      <a href="https://www.twitter.com">
-        <img src={twitterLogo} alt="Twitter" />
-        <span>Twitter</span>
+      <a href="https://github.com/TuaTuivai">
+        <img src={GithubLogo} alt="Github" />
+        <span>Github</span>
       </a>
       <a href="https://www.linkedin.com/in/tua-tuivai-0184b4216/">
         <img src={linkedinLogo} alt="LinkedIn" />
